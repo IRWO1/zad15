857b8deb7da55a520481e5032f1d088526d26935
IRWO1 <dawidekbloch@gmail.com>
2021-08-30 19:38:55 +0200

    Zadanie

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let a = document.querySelector('#dodaj').onclick = function() {
+    document.querySelector('p').textContent = "Oto paragraf";
+}
+let b = document.querySelector('#usun').onclick = function() {
+    document.querySelector('p').textContent = "";
+}
